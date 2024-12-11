7eb3e2070b2ce6187da06ec90bfd134d62ad9e19
fireleit <fireleit@gmail.com>
2024-12-11 15:56:41 +0400

    feat: add main strucyure

--- js/slider.js
+++ js/slider.js
@@ -1,67 +1,24 @@
-const reviewsSlider = new Swiper('.reviews-slider', {
-    // Optional parameters
-    loop: true,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.slider-nav.right',
-      prevEl: '.slider-nav.left',
+
+const casesSlider = new Swiper('.cases-slides', {
+  // Optional parameters
+  loop: true,
+  slidesPerGroup: 1,
+  slidesPerView: 3,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.cases-slider-nav.right',
+    prevEl: '.cases-slider-nav.left',
+  },
+  breakpoints: {
+    769: {
+      slidesPerView: 3,
+      slidesPerGroup: 1
     },
-  
-  });
-  
-  const productSlider = new Swiper('.product-slides', {
-    // Optional parameters
-    loop: true,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.product-slider-nav.right',
-      prevEl: '.product-slider-nav.left',
-    },
-  
-  });
-  
-  
-  const optionsSlider = new Swiper('.options-slides', {
-    // Optional parameters
-    loop: true,
-    slidesPerGroup: 1,
-    slidesPerView: 3,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.options-slider-nav.right',
-      prevEl: '.options-slider-nav.left',
-    },
-  
-  });
-  
-  const rewardsSlider = new Swiper('.rewards-slides', {
-    // Optional parameters
-    loop: true,
-    slidesPerGroup: 1,
-    slidesPerView: 4,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.rewards-slider-nav.right',
-      prevEl: '.rewards-slider-nav.left',
-    },
-  
-  });
-  
-  const sertificatsSlider = new Swiper('.sertificats-slides', {
-    // Optional parameters
-    loop: true,
-    slidesPerGroup: 1,
-    slidesPerView: 4,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.sertificats-slider-nav.right',
-      prevEl: '.sertificats-slider-nav.left',
-    },
-  
-  });
-  
+    0: {
+      slidesPerGroup: 1,
+      slidesPerView: 2
+    }
+  }
+});
+
