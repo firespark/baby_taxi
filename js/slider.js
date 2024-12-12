34997d1d8fbe800e7f0ddd065cb7c76097aa18a7
fireleit <fireleit@gmail.com>
2024-12-12 11:07:32 +0400

    feat: add css to nannies block

--- js/slider.js
+++ js/slider.js
@@ -22,3 +22,20 @@ const casesSlider = new Swiper('.cases-slides', {
   }
 });
 
+const nanniesSlider = new Swiper('.nannies-slides', {
+  // Optional parameters
+  loop: true,
+  slidesPerGroup: 1,
+  slidesPerView: 4,
+
+  breakpoints: {
+    769: {
+      slidesPerView: 4,
+      slidesPerGroup: 1
+    },
+    0: {
+      slidesPerGroup: 1,
+      slidesPerView: 2
+    }
+  }
+});
